1380bc0a4ccbeadebbbb3d5ddc8cc687aa3aee55
Junxin Zheng - Gin <junxin.zheng0001@stud.hkr.se>
2023-10-08 14:10:54 +0200

    ci: :construction_worker: Disable Standard JS Linting

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require("express");
-const app = express();
-const port = 8000;
+const express = require('express')
+const app = express()
+const port = 8000
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}.`);
-});
+  console.log(`Server listening on port ${port}.`)
+})
