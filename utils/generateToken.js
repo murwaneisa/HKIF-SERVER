4a4f88d640388df5660ccb378019120bb2b11bb9
Junxin Zheng - Gin <junxin.zheng0001@stud.hkr.se>
2023-10-09 15:06:38 +0200

    feat: :sparkles: Completes token generation, starts part of token verification.

---
+++ utils/generateToken.js
@@ -0,0 +1,31 @@
+import jwt from 'jsonwebtoken'
+
+export const generateAccessToken = (info, type) => {
+  const payload = {
+    _id: info._id,
+    ...(type === 'user' ? { membershipType: info.membershipType } : {}),
+    ...(type === 'admin' ? { role: info.role } : {}),
+  }
+  return jwt.sign(payload, process.env.JWT_SECRET, {
+    expiresIn: '15m',
+  })
+}
+
+export const generateRefreshToken = (info, type) => {
+  const payload = {
+    _id: info._id,
+    ...(type === 'user' ? { membershipType: info.membershipType } : {}),
+    ...(type === 'admin' ? { role: info.role } : {}),
+    iat: Math.floor(Date.now() / 1000), // Issued at
+    exp: Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 30 * 7, // Expires in 210 days
+    jti: generateJti(), // JWT ID
+  }
+
+  return jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET)
+}
+
+function generateJti() {
+  // Generate a unique JWT ID. This could be a random string.
+  // Ensure it's sufficiently random and complex to be unguessable.
+  return crypto.randomBytes(16).toString('hex')
+}
