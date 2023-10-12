0f7a3eaad91fac54d6253437ef1c230857fc5a6c
ahmedmohd957 <ahmed.mohd957@gmail.com>
2023-10-12 11:31:02 +0200

    code update: removed app.get

--- server.js
+++ server.js
@@ -4,15 +4,11 @@ require('dotenv').config()
 const db = require('./configs/db')
 const userRouter = require('./routes/userRoutes')
 
-db.connectToDB()
-
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
 app.use('/user', userRouter)
+
+db.connectToDB()
 
 app.listen(process.env.PORT, () => {
   console.log(`Server listening on port ${process.env.PORT}.`)
