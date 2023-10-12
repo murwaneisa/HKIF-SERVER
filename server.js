f4f07442d8339815059589840c0f5fb52f953975
ahmedmohd957 <ahmed.mohd957@gmail.com>
2023-10-12 17:55:47 +0200

    activity_leader routes added

--- server.js
+++ server.js
@@ -5,12 +5,14 @@ const db = require('./configs/db')
 const activityRoutes = require('./routes/activityRoutes')
 const eventRoutes = require('./routes/eventRoutes')
 const boardMemberRoutes = require('./routes/baordMemberRoutes')
+const activityLeaderRoutes = require('./routes/activityLeaderRoutes')
 
 app.use(express.json())
 
 app.use('/activities', activityRoutes)
 app.use('/events', eventRoutes)
 app.use('/boardMembers', boardMemberRoutes)
+app.use('/activityLeader', activityLeaderRoutes)
 
 db.connectToDB()
 
