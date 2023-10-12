7ca5d83d73abff2ab99f90cbfa7262ba0724c9e7
ahmedmohd957 <ahmed.mohd957@gmail.com>
2023-10-12 11:20:07 +0200

    implementation for create method

--- controllers/activityController.js
+++ controllers/activityController.js
@@ -3,16 +3,23 @@ const Activity = require('../models/Activity')
 exports.getAll = async (req, res) => {
   try {
     const activities = await Activity.find()
-    console.log(activities)
     res.json(activities)
   } catch (err) {
-    console.log(err)
+    res.status(500).json({ message: 'Internal Server Error', err })
   }
 }
 
 exports.getById = async (req, res) => {}
 
-exports.create = async (req, res) => {}
+exports.create = async (req, res) => {
+  try {
+    const newActivity = new Activity(req.body)
+    await newActivity.save()
+    res.status(201).json({ message: 'Activity created successfully' })
+  } catch (err) {
+    res.status(500).json({ message: 'Internal Server Error', err })
+  }
+}
 
 exports.update = async (req, res) => {}
 
