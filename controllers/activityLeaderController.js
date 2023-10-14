2e94781bd10703bd85f07320becf7d356225e4f4
Jasmin Akter Chowdhury <jasmine.chy12223@gmail.com>
2023-10-14 13:23:11 +0200

    Activity Leaders routes done

--- controllers/activityLeaderController.js
+++ controllers/activityLeaderController.js
@@ -1,11 +1,72 @@
 const ActivityLeader = require('../models/ActivityLeader')
 
-exports.getAll = async (req, res) => {}
+exports.getAll = async (req, res) => {
+  try {
+    const activityLeader = await ActivityLeader.find()
+    console.log(activityLeader)
+    res.json(activityLeader)
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({ message: 'Server error' });
+  }
+}
 
-exports.getById = async (req, res) => {}
+exports.getById = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const activityLeader = await ActivityLeader.findById(id);
+    if (!activityLeader) {
+      return res.status(404).json({ message: 'ActivityLeader not found' });
+    }
+    console.log(activityLeader);
+    res.json(activityLeader);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: 'Server error' });
+  }
+}
 
-exports.create = async (req, res) => {}
+exports.create = async (req, res) => {
+  try {
+    const { firstName, lastName, email, imageUrl } = req.body;
+    console.log(req.body);
+    const newActivityLeader = new ActivityLeader({ firstName, lastName, email, imageUrl });
+    await newActivityLeader.save();
+    res.status(201).json(newActivityLeader);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: 'Server error' });
+  }
+}
 
-exports.update = async (req, res) => {}
+exports.update = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const { firstName, lastName, imageUrl, activityId } = req.body;
+    console.log( firstName, lastName, imageUrl, activityId);
+    const activityLeader = await ActivityLeader.findByIdAndUpdate(id, { firstName, lastName, imageUrl, activityId }, { new: true });
+    if (!activityLeader) {
+      return res.status(404).json({ message: 'ActivityLeader not found' });
+    }
+    console.log(activityLeader);
+    res.json(activityLeader);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: 'Server error' });
+  }
+}
 
-exports.remove = async (req, res) => {}
+exports.remove = async (req, res) => {
+  try {
+    const id = req.params.id; 
+    const activityLeader = await ActivityLeader.findByIdAndRemove(id);
+    if (!activityLeader) {
+      return res.status(404).json({ message: 'ActivityLeader not found' });
+    }
+    console.log('deleted: ', activityLeader);
+    res.json({ message: 'ActivityLeader deleted' });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: 'Server error' });
+  }
+}
