f9aedaa47520a221ed62a9c4ac58ac2100a9f944
Junxin Zheng - Gin <junxin.zheng0001@stud.hkr.se>
2023-10-14 11:27:29 +0200

    refactor: :art: Resolve jscpd issue for duplication

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,27 +1,29 @@
 const jwt = require('jsonwebtoken')
 const crypto = require('crypto')
 
-exports.generateAccessToken = (info, type) => {
-  const payload = {
+const generatePayload = (info, type, additionalData = {}) => {
+  return {
     _id: info._id,
     ...(type === 'user' ? { membershipType: info.membershipType } : {}),
     ...(type === 'admin' ? { role: info.role } : {}),
+    ...additionalData,
   }
+}
+
+const generateAccessToken = (info, type) => {
+  const payload = generatePayload(info, type)
   return jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {
     expiresIn: '15m',
   })
 }
 
-exports.generateRefreshToken = (info, type) => {
-  const payload = {
-    _id: info._id,
-    ...(type === 'user' ? { membershipType: info.membershipType } : {}),
-    ...(type === 'admin' ? { role: info.role } : {}),
+const generateRefreshToken = (info, type) => {
+  const additionalData = {
     iat: Math.floor(Date.now() / 1000), // Issued at
     exp: Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 30 * 7, // Expires in 210 days
     jti: generateJti(), // JWT ID
   }
-
+  const payload = generatePayload(info, type, additionalData)
   return jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET)
 }
 
