455d72b7f36de075bbf603c3618cd0ceaa04a475
Junxin Zheng - Gin <junxin.zheng0001@stud.hkr.se>
2024-02-15 13:54:41 +0100

    feat: :sparkles: Implements unpaid users deletion.

--- controllers/userController.js
+++ controllers/userController.js
@@ -300,3 +300,25 @@ exports.resetPassword = async (req, res) => {
     res.status(500).json({ message: 'Internal Server Error' })
   }
 }
+
+exports.deleteUser = async (req, res) => {
+  try {
+    const userId = req.params.id
+    const user = await User.findById(userId)
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' })
+    }
+
+    if (user.membershipType !== 'UNPAID') {
+      return res
+        .status(403)
+        .json({ message: 'Only UNPAID users can be deleted by this method' })
+    }
+
+    await User.deleteOne({ _id: userId })
+    res.json({ message: 'User deleted successfully' })
+  } catch (err) {
+    res.status(500).json({ error: err.message })
+  }
+}
